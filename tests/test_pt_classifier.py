025ff689c4a66dcc4112c99c6ecf4d09d13ded7e
Nicolas Pannetier <nicolaspannetier@flywheel.io>
2020-04-27 17:39:44 -0700

    add test for pet classifier

---
+++ tests/test_pt_classifier.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from PT_classifier import classify_PT
+import flywheel
+
+
+def test_classify_PT():
+    dcm_metadata = {
+        'info': {'header': {'dicom': {
+                    'SeriesDescription': '[WB_CTAC] Body',
+                    'ImageType': ['ORIGINAL', 'PRIMARY'],
+                    'CorrectedImage': ['DECY', 'RADL', 'ATTN', 'SCAT', 'DTIM', 'RAN', 'NORM'],
+                    'RadiopharmaceuticalInformationSequence': [{
+                        'RadionuclideCodeSequence': [{'CodeValue': 'C-111A1'}],
+                        'RadiopharmaceuticalCodeSequence': [{'CodeValue': 'C-B1031'}]
+                    }]
+        }}}}
+    ipp = [[0, 0, a] for a in range(100)]
+    df = pd.DataFrame({'ImagePositionPatient': ipp})
+    acquisition = flywheel.Acquisition(label='Neck')
+    res = classify_PT(df, dcm_metadata, acquisition)
+    assert res['classification']['Isotope'] == ['F18']
+    assert res['classification']['Processing'] == ['Attenuation Corrected']
+    assert res['classification']['Tracer'] == ['FDG']
