2e4312c7625eeb9a10459898610c52bfc13b7f18
Jesus Avila <avila.jesus@gene.com>
2020-05-08 08:45:28 -0700

    Changed nested logic for 'ImageType' and 'ImagePositionPatient' checks to flat logic tests

--- common_utils.py
+++ common_utils.py
@@ -4,61 +4,61 @@ import ast
 
 log = logging.getLogger(__name__)
 
+
 # Scan Coverage
 def compute_scan_coverage(df):
     log.info(
         f"Attempting to compute scan coverage..."
     )
-    scan_coverage = None
     dicom_std_link = \
         "https://dicom.innolitics.com/ciods/pet-image/image-plane/00200032"
-    if 'ImagePositionPatient' in df.keys():
-        image_positions = df['ImagePositionPatient']
-
-        # Check if all image positions are not None. Log error.
-        if all(image_positions.isnull().apply(lambda x: not x)):
-            # Check if all values are type list. Log type error if not.
-            if all(image_positions.apply(lambda x: type(x) == list)):
-                # Check if all lists are at least length 3. Log missing
-                # values error if not.
-                if all(image_positions.apply(lambda x: len(x) == 3)):
-                    # Check that all values in the z axis are type float
-                    if all(image_positions.apply(lambda x:
-                                                 type(x[2]) == float)):
-
-                        # Compute scan coverage if all conditions are met
-                        z_position = image_positions.apply(lambda x: x[2])
-                        result = z_position.max() - z_position.min()
-                        scan_coverage = result if result > 0 else result * -1
-                        log.info(
-                            f"Computed scan coverage ({scan_coverage})"
-                        )
-                    else:
-                        log.error(
-                            f"Cannot compute scan coverage. Some or all "
-                            f"'ImagePositionPatient' z-axis values of dicom "
-                            f"slices are not type 'float'.")
-                else:
-                    log.error(
-                        f"Cannot compute scan coverage. Some or all "
-                        f"'ImagePositionPatient' values of dicom slices "
-                        f"are not length 3. This is required. See"
-                        f" {dicom_std_link}")
-            else:
-                log.error(
-                    f"Cannot compute scan coverage. Some or all "
-                    f"'ImagePositionPatient' values are not type 'list'.")
-        else:
-            log.error(
-                f"Cannot compute scan coverage.. Some or all "
-                f"'ImagePositionPatient' values of dicom slices "
-                f"are missing. This is required. See {dicom_std_link}")
-
-    else:
+    if 'ImagePositionPatient' not in df.keys():
         log.error(
             f"Cannot compute scan coverage. 'ImagePositionPatient' not in "
             f"dataframe (dicom headers). This is required. See "
             f"{dicom_std_link}")
+        return None
+
+    image_positions = df['ImagePositionPatient']
+
+    # Check if all image positions are not None. Log error.
+    if not all(image_positions.isnull().apply(lambda x: not x)):
+        log.error(
+            f"Cannot compute scan coverage.. Some or all "
+            f"'ImagePositionPatient' values of dicom slices "
+            f"are missing. This is required. See {dicom_std_link}")
+        return None
+    # Check if all values are type list. Log type error if not.
+    if not all(image_positions.apply(lambda x: type(x) == list)):
+        log.error(
+            f"Cannot compute scan coverage. Some or all "
+            f"'ImagePositionPatient' values are not type 'list'.")
+            # Check if all lists are at least length 3. Log missing
+            # values error if not.
+        return None
+    if not all(image_positions.apply(lambda x: len(x) == 3)):
+        log.error(
+            f"Cannot compute scan coverage. Some or all "
+            f"'ImagePositionPatient' values of dicom slices "
+            f"are not length 3. This is required. See"
+            f" {dicom_std_link}")
+        return None
+    # Check that all values in the z axis are type float
+    if not all(image_positions.apply(lambda x:
+                                 type(x[2]) == float)):
+        log.error(
+            f"Cannot compute scan coverage. Some or all "
+            f"'ImagePositionPatient' z-axis values of dicom "
+            f"slices are not type 'float'.")
+        return None
+
+    # Compute scan coverage if all conditions are met
+    z_position = image_positions.apply(lambda x: x[2])
+    result = z_position.max() - z_position.min()
+    scan_coverage = result if result > 0 else result * -1
+    log.info(
+        f"Computed scan coverage ({scan_coverage})")
+
     return scan_coverage
 
 
@@ -94,7 +94,6 @@ def is_localizer(label):
 
 
 def compute_scan_coverage_if_original(header_dicom, df, info_object):
-    scan_coverage = None
     log.info(
         f"Checking if header 'ImageType' == 'ORIGINAL' to determine if scan "
         f"coverage should be computed..."
@@ -105,57 +104,43 @@ def compute_scan_coverage_if_original(header_dicom, df, info_object):
     dicom_std_link = \
         "https://dicom.innolitics.com/ciods/cr-image/general-image/00080008"
     image_type = header_dicom.get('ImageType')
-    if image_type:
-        is_original = False
-        if type(image_type) is list:
-            if image_type[0] == 'ORIGINAL':
-                is_original = True
-            elif image_type[0] == 'DERIVED':
-                log.info(f"Cannot computed scan coverage. 'ImageType' is "
-                         f"{image_type[0]}")
-            else:
-                log.error(
-                    f"Cannot determine if scan coverage should be computed. "
-                    f"Dicom header 'ImageType[0]' ({image_type[0]}) is not "
-                    f"'ORIGINAL' or 'DERIVED'. See {dicom_std_link}"
-                )
-        elif type(image_type) is str:
-            log.warning(
-                f"'ImageType' in dicom header is not a list but a string "
-                f"({image_type}). This does not follow the dicom standard. "
-                f"See {dicom_std_link}"
-            )
-            if image_type == 'ORIGINAL':
-                is_original = True
-            elif image_type == 'DERIVED':
-                log.info(f"Cannot compute scan coverage. 'ImageType' is "
-                         f"{image_type}")
-            else:
-                log.warning(
-                    f"Cannot determine if scan coverage should be computed. "
-                    f"Dicom header 'ImageType' ({image_type}) is not "
-                    f"'ORIGINAL' or 'DERIVED'. See {dicom_std_link}"
-                )
-        else:
-            # Throw error
-            log.error(
-                f"Cannot determine if scan coverage should be computed. "
-                f"Dicom header 'ImageType' is not a list or string, type is "
-                f"{type(image_type)})")
 
-        if is_original:
-            scan_coverage = compute_scan_coverage(df)
-    else:
+    # Check if empty
+    if not image_type:
         log.warning(
             f"Cannot determine if scan coverage should be computed. Could "
             f"not find 'ImageType' in dicom header {header_dicom}"
         )
+        return None, info_object
+
+    # Check type is list
+    if type(image_type) is not list:
+        # Throw error
+        raise TypeError(
+            f"Cannot determine if scan coverage should be computed. "
+            f"Dicom header 'ImageType' is not a list, type is "
+            f"{type(image_type)}). Try re-running latest GRP-3 "
+            f"metadata import and validation gear.")
+
+    # Check if not original or derived
+    if (image_type[0] is not 'ORIGINAL') or (image_type is not 'DERIVED'):
+        log.error(
+            f"Cannot determine if scan coverage should be computed. "
+            f"Dicom header 'ImageType[0]' ({image_type[0]}) is not "
+            f"'ORIGINAL' or 'DERIVED'. See {dicom_std_link}"
+        )
+        return None, info_object
+
+    # Check if it's 'DERIVED'
+    if image_type[0] == 'DERIVED':
+        log.info(f"Cannot computed scan coverage. 'ImageType' is "
+                 f"{image_type[0]}")
+        return None, info_object
+
+    # Compute if all checks are passed
+    scan_coverage = compute_scan_coverage(df)
 
     if scan_coverage:
         info_object['ScanCoverage'] = scan_coverage
 
     return scan_coverage, info_object
-
-
-
-
