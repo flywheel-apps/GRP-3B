d68b7fab6454cf4272534509c991eafabdab49cf
Aleksandr Dumay <56175257+aldum1@users.noreply.github.com>
2020-09-17 11:22:48 -0700

    Added logick for EyeCore. Modified Classification to accommodate new standard for Fundus photography  (#36)
    * Add logic for EyeCore to OPHTHA classifier
    * Update of the OPHTHA classifier
    
    Co-authored-by: Aleksandr Dumay <dumay.aleksandr@gene.com>

--- OPHTHA_classifier.py
+++ OPHTHA_classifier.py
@@ -5,56 +5,153 @@ import logging
 log = logging.getLogger(__name__)
 
 # Laterality, Left
-
-
 def is_left(description):
+    """
+    # return false
+    >>> is_left('some string')
+    False
+    
+    # returns true
+    >>> is_left('left')
+    True
+
+    >>> is_left('abc_le_123.jpg')
+    True
+
+    >>> is_left('abc_OS_123.jpg')
+    True
+
+    >>> is_left('abc_OD_123.jpg')
+    False
+
+    """
+
     regexes = [
-        re.compile('(^|[^a-zA-Z])(L|LE)([^a-zA-Z]|$)', re.IGNORECASE),  # L or LE not surrounded by any other letters
+        re.compile('(^|[^a-zA-Z])(L|LE)([^a-zA-Z]|$)', re.IGNORECASE),
+        # L or LE not surrounded by any other letters
+        re.compile('(^|[^a-zA-Z])(OS)([^a-zA-Z]|$)', re.IGNORECASE),
+        # OS not surrounded by any other letters
         re.compile('LEFT', re.IGNORECASE)
     ]
     return common_utils.regex_search_label(regexes, description)
 
 # Laterality, Right
-
-
 def is_right(description):
+    """
+    # return false
+    >>> is_right('some string')
+    False
+    
+    # returns true
+    >>> is_right('right')
+    True
+
+    >>> is_right('abc_re_123.jpg')
+    True
+
+    >>> is_right('abc_OD_123.jpg')
+    True
+
+    >>> is_right('abc_OS_123.jpg')
+    False
+
+    """
     regexes = [
-        re.compile('(^|[^a-zA-Z])(R|RE)([^a-zA-Z]|$)', re.IGNORECASE),  # R or RE not surrounded by any other letters
+        re.compile('(^|[^a-zA-Z])(R|RE)([^a-zA-Z]|$)', re.IGNORECASE),
+        # R or RE not surrounded by any other letters
+        re.compile('(^|[^a-zA-Z])(OD)([^a-zA-Z]|$)', re.IGNORECASE),
+        # OD not surrounded by any other letters
         re.compile('RIGHT', re.IGNORECASE)
     ]
     return common_utils.regex_search_label(regexes, description)
 
 # Modality, OCT
-
-
 def is_OCT(description):
     regexes = [
-        re.compile('OCT', re.IGNORECASE)
+        re.compile('OCT', re.IGNORECASE),
+        #for Eyecor - Start with OP_ or OPT_
+        re.compile('^OPT?_', re.IGNORECASE)
     ]
     return common_utils.regex_search_label(regexes, description)
 
 # Modality, OCT-OP
-
-
 def is_OCT_OP(description):
     regexes = [
-        re.compile('SD.*OCT.*OP(?!T)', re.IGNORECASE)   # match ...OP, but not ...OPT
+        # match ...OP, but not ...OPT
+        re.compile('SD.*OCT.*OP(?!T)', re.IGNORECASE)       
     ]
     return common_utils.regex_search_label(regexes, description)
 
 # Modality, OCT-OPT
-
-
 def is_OCT_OPT(description):
     regexes = [
-        re.compile('SD.*OCT.*OPT', re.IGNORECASE)
+        re.compile('SD.*OCT.*OPT', re.IGNORECASE),
+        #for Eyecor - Start with OPT_
+        re.compile('^OPT_', re.IGNORECASE)
     ]
     return common_utils.regex_search_label(regexes, description)
 
+# For EyeKore
+# Determine Procedure Name
+def getProtocolName(single_header_object):
+    protocolName = None
+    if 'ProtocolName' in single_header_object.keys():
+            protocolName = single_header_object['ProtocolName']
+    return protocolName
+
+# The commented blocks below are for Future development
+# def is_FA(dicom_filepath):
+#     regexes = [
+#         #for Eyecor - has with /FA/ in the path
+#         re.compile('\/FA\/', re.IGNORECASE)
+#     ]
+#     return common_utils.regex_search_label(regexes, description)
+
+# def is_FAF(dicom_filepath):
+#     regexes = [
+#         #for Eyecor - has with /FAF/ in the path
+#         re.compile('\/FAF\/', re.IGNORECASE)
+#     ]
+#     return common_utils.regex_search_label(regexes, description)
+
+# def is_COLOR(dicom_filepath):
+#     regexes = [
+#         #for Eyecor - has with /COLOR/ in the path
+#         re.compile('\/COLOR\/', re.IGNORECASE)
+#     ]
+#     return common_utils.regex_search_label(regexes, description)        
+
+# def is_ICG(dicom_filepath):
+#     regexes = [
+#         # for Eyecor - has with /ICG/ in the path
+#         re.compile('\/ICG\/', re.IGNORECASE)
+#     ]
+#     return common_utils.regex_search_label(regexes, description) 
+
+# def is_SD_OCT(dicom_filepath):
+#     regexes = [
+#         #for Eyecor - has /SD-OCT/ in the path
+#         re.compile('\/SD-OCT\/', re.IGNORECASE)
+#     ]
+#     return common_utils.regex_search_label(regexes, description)         
+
+# def get_Modality(dicom_filepath):
+#     if is_FA(dicom_filepath):
+#         modality = ['FP']
+#         modalityType = ['Fluorescein Angiography']
+#     elif is_FAF(dicom_filepath):
+#         modality = ['FP']
+#         modalityType = ['Autofluorescence']
+#     elif is_COLOR(dicom_filepath):
+#         modality = ['FP']
+#         modalityType = ['Color']
+#     elif is_ICG:
+#         modality = ['FP']
+#         modalityType = ['Indocyanine Green']
+#     return modality, modalityType    
+
 ######################################################################################
 ######################################################################################
-
-
 def classify_OPHTHA(dcm_metadata, acquisition):
     """
     Classifies a OCT dicom series
@@ -62,78 +159,152 @@ def classify_OPHTHA(dcm_metadata, acquisition):
         dcm_metadata:  dicom data object
         acquisition:  acquisition object
     Returns:
-        dcm_metadata: The dictionary for the OCT classification 
+        dcm_metadata: The dictionary for the OCT classification
     """
+
     log.info("Determining OPHTHA Classification...")
     single_header_object = dcm_metadata['info']['header']['dicom']
-
     updateFlag = False
+    protocolName = None
+    octType = None
+    subType = None
+    modality = None
+    modalityType = None
 
     if 'Columns' in single_header_object.keys():
         updateFlag = True
-    elif  is_OCT(acquisition.label):
-        updateFlag = True   
-    
+    elif is_OCT(acquisition.label):
+        updateFlag = True
+
     if updateFlag:
         study_description = None
         if 'StudyDescription' in single_header_object.keys():
             study_description = single_header_object['StudyDescription']
-        
         device_code_sequence = None
         if 'AcquisitionDeviceTypeCodeSequence' in single_header_object.keys():
             if 'CodeValue' in single_header_object['AcquisitionDeviceTypeCodeSequence']:
-                device_code_sequence = single_header_object['AcquisitionDeviceTypeCodeSequence']['CodeValue']  
+                device_code_sequence = single_header_object['AcquisitionDeviceTypeCodeSequence']['CodeValue']
             elif 'CodeValue' in single_header_object['AcquisitionDeviceTypeCodeSequence'][0]:
-                device_code_sequence = single_header_object['AcquisitionDeviceTypeCodeSequence'][0]['CodeValue']  
-
+                device_code_sequence = single_header_object['AcquisitionDeviceTypeCodeSequence'][0]['CodeValue']
         classifications = {}
+        
+        # Get ProtocolName - used in EyeCore
+        protocolName = getProtocolName(single_header_object)
 
         # Get Modality
-        modality = None
-        if device_code_sequence and device_code_sequence == 'A-00FBE':
+        if protocolName:
+            if protocolName == 'FA':
+                modality = 'FP'
+                modalityType = 'Fluorescein Angiography'
+                subType = 'Standard Field'
+            elif protocolName == 'FA-4W Sweep':
+                modality = 'FP'
+                modalityType = 'Fluorescein Angiography'
+                subType = 'Wide Field'
+            elif protocolName == 'FA: UltraWidefield':
+                modality = 'FP'
+                modalityType = 'Fluorescein Angiography'
+                subType = 'Ultra-wide Field' 
+            elif protocolName == 'FA6':
+                modality = 'FP'
+                modalityType = 'Fluorescein Angiography'
+            elif protocolName == 'FAF':
+                modality = 'FP'
+                modalityType = 'Autofluorescence'
+            elif protocolName in ['FP','FP-2','FP-3M','FP-4W','FP-7M','FP-7Std','FP-9','FP-ROP']:
+                modality == 'FP'
+                modalityType == 'Color'
+                subType = 'Standard'  
+            elif protocolName == 'ICG':
+                modality = 'FP'
+                modalityType = 'Indocyanine Green'
+            elif protocolName == 'OCT Angiography':
+                modality = 'OCT'
+                octType = ['Angiography']
+            elif protocolName == 'SD-OCT':
+                modality = 'OCT'
+                octType = ['Standard']
+            elif protocolName == 'UWF-AF':
+                modality = 'FP'
+                modalityType = 'Autofluorescence' 
+                subType = 'Ultra-wide Field'  
+            elif protocolName == 'UWF-C':
+                modality = 'FP'
+                modalityType = 'Color' 
+                subType = 'Ultra-wide Field' 
+            elif protocolName == 'UWF-ICG':
+                modality = 'FP'
+                modalityType = 'Indocyanine Green' 
+                subType = 'Ultra-wide Field'
+            elif protocolName == 'Widefield OCT':
+                modality = 'OCT'
+                octType = ['Standard'] 
+                octSubType = 'Wide Field'
+        elif device_code_sequence and device_code_sequence == 'A-00FBE':
             modality = 'OCT'
         elif is_OCT(acquisition.label):
             modality = 'OCT'
             updateFlag = True
         elif device_code_sequence and study_description and study_description == 'CF':
-            modality = 'CF'
+            modality = 'FP'
+            modalityType = 'Color'
 
+        if modality:
+            updateFlag = True 
+            if modalityType:
+                classifications['Type']=[modalityType]
+            if subType:
+                classifications['Sub-Type'] = [subType]
+ 
 
+        
         # Get Laterality
         laterality = None
-        if single_header_object.get('ImageLaterality') == 'R':
+        if single_header_object.get('ImageLaterality') in ('R','OD'):
             laterality = ['Right Eye']
-        elif single_header_object.get('ImageLaterality') == 'L':
+        elif single_header_object.get('ImageLaterality') in ('L','OS'):
             laterality = ['Left Eye']
         elif is_right(acquisition.label):
             laterality = ['Right Eye']
         elif is_left(acquisition.label):
             laterality = ['Left Eye']
+        
         if laterality:  # set classification laterality
-            classifications.update({"Laterality": laterality})
-
+            classifications['Laterality'] = laterality
+            # classifications.update({"Laterality": laterality}) update later using Dictionary
+        
         # Get OCT Type
-        oct_type = None
-        if device_code_sequence and device_code_sequence == 'A-00FBE':
-            oct_type = ['Standard']
-            # print("Found match for Standard OCT from AcquisitionDeviceTypeCodeSequence match")
-        elif device_code_sequence and device_code_sequence == 'A-00E8A':
-            oct_type = ['Fundus']
-            # print("Found match for Fundus OCT from AcquisitionDeviceTypeCodeSequence match")
-        elif is_OCT_OP(acquisition.label):
-            oct_type = ['Fundus']
-            # print("Found match for Fundus OCT from is_OCT_OP match")
-        elif is_OCT_OPT(acquisition.label):
-            oct_type = ['Standard']
-            # print("Found match for Standard OCT from is_OCT_OPT match")
+        if octType is None:
+            if device_code_sequence and device_code_sequence == 'A-00FBE':
+                octType = ['Standard']
+                # print("Found match for Standard OCT from AcquisitionDeviceTypeCodeSequence match")
+            elif device_code_sequence and device_code_sequence == 'A-00E8A':
+                octType = ['Fundus']
+                # print("Found match for Fundus OCT from AcquisitionDeviceTypeCodeSequence match")
+            elif is_OCT_OP(acquisition.label):
+                octType = ['Fundus']
+                # print("Found match for Fundus OCT from is_OCT_OP match")
+            elif is_OCT_OPT(acquisition.label):
+                octType = ['Standard']
+                # print("Found match for Standard OCT from is_OCT_OPT match")
+        
+        if modality == 'OCT' and octType:
+            classifications.update({"OCT Type": octType})
 
-        if oct_type:
-            classifications.update({"OCT Type": oct_type})
         if classifications:
             dcm_metadata['classification'] = classifications
+
         if modality:
             dcm_metadata['modality'] = modality
-    else: 
-        pass        
+            dcm_metadata['type'] = modalityType
+            if subType:
+                dcm_metadata['Sub-Type'] = [subType]
 
+    else:
+        pass
     return dcm_metadata
+
+## Perform test if run directly
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
